6237232dc83276a9ea66c63b3a3b6011e1e8ead2
bismayesh <125033480+bismayesh@users.noreply.github.com>
2024-09-12 09:58:10 +0200

    updated enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,20 +6,39 @@ public class Enemy : MonoBehaviour
 {
      private int MaxHealth;
      private float MoveSpeed;
+    public GameObject prefab;
+    public Transform[] spawnPoint;
+    private Vector3 spawnPos;
+  
 
     private int Health;
 
     void Start()
     {
+        
         Health=MaxHealth;
+       
     }
 
     
     void Update()
     {
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            SpawnEnemy();
+        }
         //move towards the player
         //On collison minus one health
-        //if health is less the 0 the destroy enemy
+        //if health is less the 0 the destroy  if (player != null)
+        
+
+    }
+
+    public void SpawnEnemy()
+
+    {
+        Instantiate(prefab, spawnPos, Quaternion.identity);
     }
 
     public void OnCollison()
